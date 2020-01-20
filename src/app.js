141f24738ab6289d22f0017320e3aa5ed393f5ed
Your Name <you@example.com>
2020-01-20 11:22:08 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+
+const app = express()
+
+const morganOption = (process.env.NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common'
+
+app.use(morgan(morganOption))
+app.use(cors())
+app.use(helmet())
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'server error' }
+  } else {
+    console.error(error)
+    response = { error: error.message, details: error }
+  }
+  res.status(500).json(response)
+})
+
+module.exports = app
