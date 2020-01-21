90702742199c3a23946735a74e35053c60cac3f8
Your Name <you@example.com>
2020-01-21 09:42:43 -0600

    second commit

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,12 @@
-console.log('hello, practice')
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL
+})
+
+knexInstance.from('amazong_products').select('*')
+    .then(result => {
+        console.log(result)
+    })
